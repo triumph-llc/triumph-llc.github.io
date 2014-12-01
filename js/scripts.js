c928bf755f45c5f16696bbb6145af208f3d4a985
Dmitry Marusin <marusin-dv@brit.co.ru>
2014-12-01 14:25:25 +0400

    version 1.0.1

--- js/scripts.js
+++ js/scripts.js
@@ -746,7 +746,7 @@ $(document).ready(function () {
 		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 	}
 	
-	google.maps.event.addDomListener(window, 'load', initialize);
+	//google.maps.event.addDomListener(window, 'load', initialize);
 	
 });
 
